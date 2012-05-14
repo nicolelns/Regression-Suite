f8937c37ead3013ff031a6fe704dbc72eb358fc4
Nicole <nicole.smith@nymag.com>
2012-05-14 14:07:44 -0400

    Minor changes to test names

---
+++ TESTS/vulture/suites/vulture_home_regression.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+cd ../desktop/prod/rc
+python 404.rc.py
+python biginterview.rc.py
+python clickables.rc.py
+python cpfooter.rc.py
+python footer.rc.py
+python globalnav.rc.py
+python hottopics.rc.py
+python keepup.rc.py
+python latestnews.rc.py
